ae0ba8b9df6869ab4638f732f3f01cc3bfc60b66
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-08-24 00:37:58 +0700

    Implement database connection and configuration, add Docker Compose for PostgreSQL, and initialize database schema

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/AndreasNugoho/go-social/internal/db"
 	"github.com/AndreasNugoho/go-social/internal/env"
 	"github.com/AndreasNugoho/go-social/internal/store"
 )
@@ -10,9 +11,30 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":9090"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+
+	log.Printf("Database connection established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
---
+++ internal/db/db.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"time"
+	// "golang.org/x/net/context"
+)
+
+func New(addr string, maxOpenConns, maxIdleConns int, maxIdlleTime string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+
+	duration, err := time.ParseDuration(maxIdlleTime)
+
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
