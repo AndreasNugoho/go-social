63912229f76cbdd4eac0da557a03780153733db6
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-08-23 23:53:40 +0700

    Add Post and User models with storage functionality
    - Implement Post and UsersStore types for handling posts and users in the database.
    - Add Create methods for both Poststore and UsersStore to insert new records.
    - Update main application to utilize the new storage structure.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/AndreasNugoho/go-social/internal/env"
+	"github.com/AndreasNugoho/go-social/internal/store"
 )
 
 func main() {
@@ -11,8 +12,11 @@ func main() {
 		addr: env.GetString("ADDR", ":9090"),
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/AndreasNugoho/go-social
 go 1.24.2
 
 require (
-	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.2.2
+	github.com/lib/pq v1.10.9
 )
